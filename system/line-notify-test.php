27d81441559dd7a5c1935ed3d1de940d0617bb18
DIONYSUS\armepavong <partyparti@gmail.com>
2018-03-22 18:43:21 +0700

    no message

--- system/line-notify-test.php
+++ system/line-notify-test.php
@@ -1,6 +1,4 @@
 <?php 
-header('Content-Type: application/json');
-
 $user_id = "U9d9ea40b1b06d2518a9bd6e64e74147d";
 $access_token = "q+wPquCFdGY2ZqRj/BBpS95kouLlO2OdKHjshfccPcMHeC8XALFW/nI6V1/ZKNcz1KehUU6mlOzom9JT0TNGqirxQV3HS/VrVodntXYsMzdozLsfnsahYwbH4oZoeMOj/0U6EVWC/PA64+9l4ZrcOwdB04t89/1O/w1cDnyilFU=";
 $channel_secret = "7596dca412ced79ae5c8b72feda17c52";
@@ -12,7 +10,7 @@ $messages = array(
     )
 );
 
-$parameter = array(
+$parameters = array(
     "to" => $user_id,
     "messages" => $messages
 );
@@ -23,15 +21,13 @@ curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => json_encode($parameter),
+    CURLOPT_POSTFIELDS => json_encode($parameters),
     CURLOPT_HTTPHEADER => array(
         "Content-Type: application/json",
         "Authorization: Bearer ".$access_token
     ),
 ));
 $response_send = json_decode(curl_exec($curl));
+var_dump($response_send);
 curl_close($curl);
-
-echo json_encode($response_send);
-
 ?>
