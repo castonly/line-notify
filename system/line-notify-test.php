fd59060e5b10e01f946b89c4b09c00a087d51282
DIONYSUS\armepavong <partyparti@gmail.com>
2018-03-22 17:36:35 +0700

    no message

--- system/line-notify-test.php
+++ system/line-notify-test.php
@@ -1,13 +1,15 @@
 <?php 
+require("LINE/LINEBot.php");
+
 $user_id = "U9d9ea40b1b06d2518a9bd6e64e74147d";
 $access_token = "q+wPquCFdGY2ZqRj/BBpS95kouLlO2OdKHjshfccPcMHeC8XALFW/nI6V1/ZKNcz1KehUU6mlOzom9JT0TNGqirxQV3HS/VrVodntXYsMzdozLsfnsahYwbH4oZoeMOj/0U6EVWC/PA64+9l4ZrcOwdB04t89/1O/w1cDnyilFU=";
 $channel_secret = "7596dca412ced79ae5c8b72feda17c52";
 
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('$access_token');
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '$channel_secret']);
 
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-    $response = $bot->pushMessage($user_id, $textMessageBuilder);
+    $response = $bot->pushMessage('$user_id', $textMessageBuilder);
 
     echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
