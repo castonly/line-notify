143b1962a7dfb5386929c20fa940d937cff29c4a
DIONYSUS\armepavong <partyparti@gmail.com>
2018-03-22 17:09:32 +0700

    no message

--- system/line-notify-test.php
+++ system/line-notify-test.php
@@ -15,7 +15,7 @@ $access_token = "q+wPquCFdGY2ZqRj/BBpS95kouLlO2OdKHjshfccPcMHeC8XALFW/nI6V1/ZKNc
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => http_build_query("to=".$user_id."&message=".$message),
+        CURLOPT_POSTFIELDS => "to=".$user_id."&message=".$message,
         CURLOPT_HTTPHEADER => array(
             "content-type: application/json",
             "Authorization: Bearer ".$access_token
