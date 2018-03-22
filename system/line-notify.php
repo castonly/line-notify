50e4ceb7fce13a53930645a04343c4a2316ff7ea
DIONYSUS\armepavong <partyparti@gmail.com>
2018-03-22 16:16:08 +0700

    no message

--- system/line-notify.php
+++ system/line-notify.php
@@ -1,8 +1,4 @@
 <?php 
-if (!isset($_SESSION)) {
-	session_start();
-}
-
 /* get AJAX function */
 if(isset($_POST["method"])){
 	$method = $_POST["method"];
@@ -13,91 +9,84 @@ if (isset($method) && $method != ""
 	&& ($method == "line_send_verification_code")
 ) {
 
+	/* get parameters for AJAX function */
+	if(isset($_POST["parameters"])){
+		$parameters = $_POST["parameters"];
+	}
+
 	if ($method == "line_send_verification_code"){
-		line_send_verification_code();
+		line_send_verification_code($_POST["parameters"]);
 	}
 }
 
-function line_send_verification_code() {
+function line_send_verification_code($access_token) {
 
-	if (isset($_SESSION["users_api_sessions"]) && !empty($_SESSION["users_api_sessions"])) {
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.line.me/oauth2/v2.1/verify?access_token=".$access_token,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+	));
+	$response_verify = json_decode(curl_exec($curl));
+	curl_close($curl);
 
-		$users_api_sessions = $_SESSION["users_api_sessions"];
-		for ($i = 0; $i < count($users_api_sessions); $i++) {
-			if ($users_api_sessions[$i]["api_name"] == "Line") {
+	if ($response_verify->expires_in > 0) {
 
-				$curl = curl_init();
-				curl_setopt_array($curl, array(
-					CURLOPT_URL => "https://api.line.me/oauth2/v2.1/verify?access_token=".$users_api_sessions[$i]["api_access_token"],
-					CURLOPT_RETURNTRANSFER => true,
-					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-					CURLOPT_CUSTOMREQUEST => "GET",
-				));
-				$response_verify = json_decode(curl_exec($curl));
-				curl_close($curl);
+		$message = array(
+			"id" => "userId",
+			"type" => "text",
+			"message" => "Your verfication code is "
+		);
 
-				if ($response_verify->expires_in > 0) {
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://api.line.me/v2/bot/message/push",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "to=".$users_api_sessions[$i]["api_details"]["userId"]."&message=".json_encode($message),
+			CURLOPT_HTTPHEADER => array(
+				"content-type: application/json",
+				"Authorization: Bearer ".$users_api_sessions[$i]["api_access_token"]
+			),
+		));
+		$response_send = json_decode(curl_exec($curl));
+		curl_close($curl);
 
-					$message = array(
-						"id" => "userId",
-						"type" => "text",
-						"message" => "Your verfication code is "
-					);
+		echo "to=".$users_api_sessions[$i]["api_details"]["userId"]."&message=".json_encode($message);
 
-					$curl = curl_init();
-					curl_setopt_array($curl, array(
-						CURLOPT_URL => "https://api.line.me/v2/bot/message/push",
-						CURLOPT_RETURNTRANSFER => true,
-						CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-						CURLOPT_CUSTOMREQUEST => "POST",
-						CURLOPT_POSTFIELDS => "to=".$users_api_sessions[$i]["api_details"]["userId"]."&message=".json_encode($message),
-						CURLOPT_HTTPHEADER => array(
-							"content-type: application/json",
-							"Authorization: Bearer ".$users_api_sessions[$i]["api_access_token"]
-						),
-					));
-					$response_send = json_decode(curl_exec($curl));
-					curl_close($curl);
+		var_dump($response_send);
 
-					echo "to=".$users_api_sessions[$i]["api_details"]["userId"]."&message=".json_encode($message);
+		if ($response_send) {
 
-					var_dump($response_send);
+			$response["status"] = true;
+			$response["message"] = translate("Successfully sent Line message");
+			$response["values"] = $parameters;
 
-					if ($response_send) {
-
-						$response["status"] = true;
-						$response["message"] = translate("Successfully sent Line message");
-						$response["values"] = $parameters;
-
-						unset($parameters);
-						$parameters = array();
-						
-					} else {
-
-						$response["status"] = false;
-						$response["message"] = translate("Unable to send Line message");
-						$response["values"] = $parameters;
-
-						unset($parameters);
-						$parameters = array();
-
-					}
-
-				} else {
-
-					$response["status"] = false;
-					$response["message"] = translate("Unable to send Line message");
-					$response["values"] = $parameters;
-
-					unset($parameters);
-					$parameters = array();
-
-				}
-
-			}
+			unset($parameters);
+			$parameters = array();
 			
+		} else {
+
+			$response["status"] = false;
+			$response["message"] = translate("Unable to send Line message");
+			$response["values"] = $parameters;
+
+			unset($parameters);
+			$parameters = array();
+
 		}
-		
+
+	} else {
+
+		$response["status"] = false;
+		$response["message"] = translate("Unable to send Line message");
+		$response["values"] = $parameters;
+
+		unset($parameters);
+		$parameters = array();
+
 	}
 
 	if (isset($con)) {
