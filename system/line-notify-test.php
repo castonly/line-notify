1eff45c997ae2f1e291526f4568767b5f26198c6
DIONYSUS\armepavong <partyparti@gmail.com>
2018-03-22 18:31:44 +0700

    no message

--- system/line-notify-test.php
+++ system/line-notify-test.php
@@ -1,17 +1,8 @@
 <?php 
-require("LINE/LINEBot.php");
 
-$user_id = "U9d9ea40b1b06d2518a9bd6e64e74147d";
-$access_token = "q+wPquCFdGY2ZqRj/BBpS95kouLlO2OdKHjshfccPcMHeC8XALFW/nI6V1/ZKNcz1KehUU6mlOzom9JT0TNGqirxQV3HS/VrVodntXYsMzdozLsfnsahYwbH4oZoeMOj/0U6EVWC/PA64+9l4ZrcOwdB04t89/1O/w1cDnyilFU=";
-$channel_secret = "7596dca412ced79ae5c8b72feda17c52";
-
-    // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('$access_token');
-    // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '$channel_secret']);
-
-    // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-    // $response = $bot->pushMessage('$user_id', $textMessageBuilder);
-
-    // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+    $user_id = "U9d9ea40b1b06d2518a9bd6e64e74147d";
+    $access_token = "q+wPquCFdGY2ZqRj/BBpS95kouLlO2OdKHjshfccPcMHeC8XALFW/nI6V1/ZKNcz1KehUU6mlOzom9JT0TNGqirxQV3HS/VrVodntXYsMzdozLsfnsahYwbH4oZoeMOj/0U6EVWC/PA64+9l4ZrcOwdB04t89/1O/w1cDnyilFU=";
+    $channel_secret = "7596dca412ced79ae5c8b72feda17c52";
 
     $tos = array(
         $user_id
@@ -29,8 +20,6 @@ $channel_secret = "7596dca412ced79ae5c8b72feda17c52";
         "messages" => $messages
     );
 
-    var_dump(json_encode($param));
-
     $curl = curl_init();
     curl_setopt_array($curl, array(
         CURLOPT_URL => "https://api.line.me/v2/bot/message/push",
@@ -46,8 +35,6 @@ $channel_secret = "7596dca412ced79ae5c8b72feda17c52";
     $response_send = json_decode(curl_exec($curl));
     curl_close($curl);
 
-    echo "to=".$user_id."&message=".json_encode($message);
-
-    var_dump($response_send);
+    echo $response_send;
 
 ?>
--- system/line-notify.php
+++ system/line-notify.php
@@ -6,7 +6,7 @@ if(isset($_POST["method"])){
 	$method = $_GET["method"];
 }
 if (isset($method) && $method != "" 
-	&& ($method == "line_send_verification_code")
+	&& ($method == "push_message")
 ) {
 
 	/* get parameters for AJAX function */
@@ -14,94 +14,36 @@ if (isset($method) && $method != ""
 		$parameters = $_POST["parameters"];
 	}
 
-	if ($method == "line_send_verification_code"){
+	if ($method == "push_message"){
 		line_send_verification_code($_POST["parameters"]);
 	}
 }
 
-function line_send_verification_code($access_token) {
+function push_message($channel_secret, $access_token, $user_id, $message) {
 
-	$curl = curl_init();
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://api.line.me/oauth2/v2.1/verify?access_token=".$access_token,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
-	));
-	$response_verify = json_decode(curl_exec($curl));
+    $parameters = array(
+        "to" => $user_id,
+        "messages" => $messages
+    );
+
+    var_dump(json_encode($param));
+
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.line.me/v2/bot/message/push",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($parameters),
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json",
+            "Authorization: Bearer ".$access_token
+        ),
+    ));
+    $response_send = json_decode(curl_exec($curl));
 	curl_close($curl);
-
-	if ($response_verify->expires_in > 0) {
-
-		$message = array(
-			"id" => "userId",
-			"type" => "text",
-			"message" => "Your verfication code is "
-		);
-
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://api.line.me/v2/bot/message/push",
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => "POST",
-			CURLOPT_POSTFIELDS => "to=".$users_api_sessions[$i]["api_details"]["userId"]."&message=".json_encode($message),
-			CURLOPT_HTTPHEADER => array(
-				"content-type: application/json",
-				"Authorization: Bearer ".$users_api_sessions[$i]["api_access_token"]
-			),
-		));
-		$response_send = json_decode(curl_exec($curl));
-		curl_close($curl);
-
-		echo "to=".$users_api_sessions[$i]["api_details"]["userId"]."&message=".json_encode($message);
-
-		var_dump($response_send);
-
-		if ($response_send) {
-
-			$response["status"] = true;
-			$response["message"] = translate("Successfully sent Line message");
-			$response["values"] = $parameters;
-
-			unset($parameters);
-			$parameters = array();
-			
-		} else {
-
-			$response["status"] = false;
-			$response["message"] = translate("Unable to send Line message");
-			$response["values"] = $parameters;
-
-			unset($parameters);
-			$parameters = array();
-
-		}
-
-	} else {
-
-		$response["status"] = false;
-		$response["message"] = translate("Unable to send Line message");
-		$response["values"] = $parameters;
-
-		unset($parameters);
-		$parameters = array();
-
-	}
-
-	if (isset($con)) {
-		stop($con);
-	}
-
-	if (isset($method) && $method == __FUNCTION__) {
-		echo json_encode($response);
-	} else {
-		return $response["values"];
-	}
-	unset($response);
-	$response = array();
-	unset($method);
-	exit();
+	
+	echo $response_send;
 
 }
 ?>
